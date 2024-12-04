b2d9c3f45749d9f652738f69dc1452e300a8f14d
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2024-12-04 17:12:30 +0600

    DZ_Diploma_WEB_Sofit

---
+++ src/main/java/com/example/sofit/SofitApplication.java
@@ -0,0 +1,13 @@
+package com.example.sofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sofit/SofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
